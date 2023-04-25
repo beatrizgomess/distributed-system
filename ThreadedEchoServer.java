5deeb3852679dda01a00ccd3df41704f7cdf017a
Beatriz Gomes <78573126+beatrizgomess@users.noreply.github.com>
2023-04-25 13:56:56 -0300

    Add files via upload

--- ThreadedEchoServer.java
+++ ThreadedEchoServer.java
@@ -1,17 +1,28 @@
+	/**
+ * @version 1.10 1997-06-27
+ * @author Cay Horstmann
+ */
+
 import java.io.*;
 import java.net.*;
+import java.util.ArrayList; 
 
-public class ThreadedEchoServer
-{  public static void main(String[] args )
-   {  int i = 1;
-      try
-      {  ServerSocket s = new ServerSocket(8189);
 
-         for (;;)
-         {  Socket incoming = s.accept( );
-            System.out.println("Spawning " + i);
+public class ThreadedEchoServer{    
+
+	static ArrayList<Socket> list = new ArrayList<Socket>();
+	public static void main(String[] args ){  
+	int i = 1;
+      	try{  
+      	
+      		ServerSocket s = new ServerSocket(8189);
+
+         	for (;;){  
+         	Socket incoming = s.accept();
+            	System.out.println("Spawning " + i);
+            	list.add(incoming);
             
-            new ThreadedEchoHandler(incoming, i).start();
+            new ThreadedEchoHandler(incoming, i,list).start();
             i++;
          }
       }
@@ -21,9 +32,16 @@ public class ThreadedEchoServer
    }
 }
 
-class ThreadedEchoHandler extends Thread
-{  public ThreadedEchoHandler(Socket i, int c)
-   { incoming = i; counter = c; }
+
+class ThreadedEchoHandler extends Thread{
+	  private Socket incoming;
+	  private int counter;
+	  private ArrayList<Socket> list;
+	   
+	public ThreadedEchoHandler(Socket i, int c ,ArrayList<Socket> l){ 
+	incoming = i; counter = c; list = l;
+			
+   }
 
    public void run()
    {  try
@@ -38,9 +56,16 @@ class ThreadedEchoHandler extends Thread
          while (!done)
          {  String str = in.readLine();
              System.out.println (" - " +str);
+             for(int j = 0; j<list.size(); j++){
+            	 Socket clientes = list.get(j);
+                 PrintWriter outTodos = new PrintWriter(clientes.getOutputStream(), true /* autoFlush */);
+                 	outTodos.println("Cliente (" + counter + "): " + str);
+                 }
+             
+             
             if (str == null) done = true;
             else
-            {  out.println("Echo (" + counter + "): " + str);
+            {  out.println("Sua mensagem (" + counter + "): " + str);
 
                if (str.trim().equals("BYE"))
                   done = true;
@@ -53,7 +78,6 @@ class ThreadedEchoHandler extends Thread
       }
    }
 
-   private Socket incoming;
-   private int counter;
-}
+ 
 
+}
